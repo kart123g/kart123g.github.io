f72b0e3728aa1cadda96fd08d8d1809e67a7993d
kart123g <46489165+kart123g@users.noreply.github.com>
2022-05-02 11:56:03 +0530

    Add files via upload

---
+++ weather_app_1/main.js
@@ -0,0 +1,122 @@
+function GetInfo() {
+
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--"
+
+    // cityBackground(newName)
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=2ef30057d1ad8da016ec829048fd8f44')
+.then(response => response.json())
+.then(data => {
+
+    //Getting the min and max values for each day
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+        //Number(1.3450001).toFixed(2); // 1.35
+    }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+    //------------------------------------------------------------
+
+    //Getting Weather Icons
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+    //------------------------------------------------------------
+    console.log(data)
+
+
+})
+
+.catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"))
+}
+
+
+
+
+
+//Getting and displaying the text for the upcoming five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
+    //------------------------------------------------------------
+function cityBackground() {
+    var citytype = document.getElementById('cityInput').value.toLowerCase();
+    if (citytype === 'Mathura' || citytype === 'jait') {
+     var body=document.getElementsByTagName("body")[0];
+    //  body.style.backgroundColor = "red";
+      document.body.style.backgroundImage = "url('./images/mathura.jpg')";
+      console.log( 'Jait' );
+    }
+    if (citytype === 'london' || citytype === 'ln' || citytype === 'bay area') {
+      // $('body').css('background-image', '../images/sf.jpg');
+      //document.getElementsByTagName("body")[0].style.backgroundColor = "pink";
+      var body=document.getElementsByTagName("body")[0];
+    //  body.style.backgroundColor = "red";
+      document.body.style.backgroundImage = "url('./images/london.webp')";
+      console.log( 'london' );
+    }
+  }
+
+
+/*
+document.getElementById("day1Min").innerHTML = Math.round(data.list[0].main.temp_min - 273.15, -2);
+document.getElementById("day2Min").innerHTML = Math.round(data.list[1].main.temp_min - 273.15, -2);
+document.getElementById("day3Min").innerHTML = Math.round(data.list[2].main.temp_min - 273.15, -2);
+document.getElementById("day4Min").innerHTML = Math.round(data.list[3].main.temp_min - 273.15, -2);
+document.getElementById("day5Min").innerHTML = Math.round(data.list[4].main.temp_min - 273.15, -2);*/
+
+/*document.getElementById("day1Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day2Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day3Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day4Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day5Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);*/
+
+/*document.getElementById("img1").src = "http://openweathermap.org/img/w/"+
+data.list[0].weather[0].icon
++".png";
+document.getElementById("img2").src = "http://openweathermap.org/img/w/"+
+data.list[1].weather[0].icon
++".png";
+document.getElementById("img3").src = "http://openweathermap.org/img/w/"+
+data.list[2].weather[0].icon
++".png";
+document.getElementById("img4").src = "http://openweathermap.org/img/w/"+
+data.list[3].weather[0].icon
++".png";
+document.getElementById("img5").src = "http://openweathermap.org/img/w/"+
+data.list[4].weather[0].icon
++".png";*/
+
+/*
+document.getElementById("day1").innerHTML = weekday[CheckDay(0)];
+document.getElementById("day2").innerHTML = weekday[CheckDay(1)];
+document.getElementById("day3").innerHTML = weekday[CheckDay(2)];
+document.getElementById("day4").innerHTML = weekday[CheckDay(3)];
+document.getElementById("day5").innerHTML = weekday[CheckDay(4)];*/
+
+/*weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";*/
